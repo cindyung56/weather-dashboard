51bb18040bd4fe4ebae0303a2df9929ee422e8b6
cindy <cindyung56@gmail.com>
2022-08-30 16:05:12 -0700

    added basic bootstrap and html

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// API key = ab3a5573ca2d878ca5a062e1e21e12e8
+
+// geolocation API
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+var apiKey = "ab3a5573ca2d878ca5a062e1e21e12e8";
